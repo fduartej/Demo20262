6d87d0742ec0c3ce28d39c6c64e01c846343fd67
Fred Duarte <fduartej@gmail.com>
2026-08-20 21:47:09 -0500

    code first

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using _20262.Models;
+
+namespace _20262.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Contacto> Contactos { get; set; }
+}
---
+++ sql/querys.sql
@@ -0,0 +1,1 @@
+SELECT * FROM t_contactos WHERE Message LIKE '%gato%';
